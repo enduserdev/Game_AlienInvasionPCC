8d97ca35622ada5b43864737f0b6ef1693387c0c
enduserdev <endusercorp@gmail.com>
2021-03-07 18:11:49 +1100

    fix syntax error

--- alien_invasion.py
+++ alien_invasion.py
@@ -38,12 +38,12 @@ class AlienInvasion:
 
     def _update_screen(self):
        """Update images on the screen , and flip to the new screen."""
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+       # Redraw the screen during each pass through the loop.
+       self.screen.fill(self.settings.bg_color)
+       self.ship.blitme()
 
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+       # Make the most recently drawn screen visible.
+       pygame.display.flip()
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
