8def1b79874177df702227a07ec1324b73067fec
enduserdev <endusercorp@gmail.com>
2021-03-14 10:55:34 +1100

    add ship speed float

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,7 +27,7 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
-            print(self.ship.moving_right)
+            print(self.ship.moving_left)
             self.ship.update()
             self._update_screen()
 
@@ -40,9 +40,13 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
+                    self.ship.moving_right = False 
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
        """Update images on the screen , and flip to the new screen."""
